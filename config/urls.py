91e0dfa9d30b0fed43f0c565296da11736023d8d
zinedine098 <ibnuwawan80@gmail.com>
2025-11-18 09:36:45 +0700

    `Added static files configuration and debug URL handling`

--- config/urls.py
+++ config/urls.py
@@ -18,7 +18,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("landing.urls")),
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
