b6dcfea2778568d20d1aad9063dac1899234f496
zinedine098 <ibnuwawan80@gmail.com>
2025-11-18 09:22:50 +0700

    Remove unused authentication, dashboard, and landing app files

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "landing",
+    "dashboard",
+    "authentication",
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,7 @@ ROOT_URLCONF = "config.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [temp := BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
